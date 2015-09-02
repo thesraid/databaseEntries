bc2bb08d33a1b5ba14b9e01032430fd861142b01
thesraid <thesraid@gmail.com>
2015-09-02 22:18:31 +0100

    adding files

--- updateGit.sh
+++ updateGit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# It's fecking complicated to upload to git
+# http://stackoverflow.com/questions/12799719/how-to-upload-a-project-to-github
 git add *
 git commit -m "adding files"
 git push origin master
